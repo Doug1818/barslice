f06031ef98e8e9952fd81842faf895816daa0eca
Doug Raicek <doug.raicek@gmail.com>
2012-06-12 21:40:00 -0400

    Delete original Bar migration files

--- db/migrate/20120611035828_add_isbar_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddIsbarToUser < ActiveRecord::Migration
-  def change
-    add_column :users, :isbar, :boolean
-  end
-end
--- db/migrate/20120611044427_add_bar_id_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddBarIdToUser < ActiveRecord::Migration
-  def change
-    add_column :users, :bar_id, :integer
-  end
-end
