d7c839587fd62dfd03d446b1687b0d20eed7fd51
David Caplan <dcaplan@gmail.com>
2012-07-26 16:59:36 -0700

    show img thumbnail in roompic_fields partial

--- app/models/hrsrange.rb
+++ app/models/hrsrange.rb
@@ -1,4 +1,4 @@
 class Hrsrange < ActiveRecord::Base
-  attr_accessible :end, :friday, :monday, :saturday, :start, :sunday, :thursday, :tuesday, :wednesday
+  attr_accessible :close, :friday, :monday, :saturday, :open, :sunday, :thursday, :tuesday, :wednesday
   belongs_to :bar
 end
