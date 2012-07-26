869c00407b14b8c47df32a5200664eb1302c27d0
Doug Raicek <doug.raicek@gmail.com>
2012-07-26 16:38:40 -0400

    Develop search form

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,17 +1,21 @@
 class Search < ActiveRecord::Base
   attr_accessible :bar_name, :date, :hdct, :location, :privacy, :start_time, :end_time
 
+  before_save { |search| search.date = Date.strptime(date,"%m/%d/%Y").to_time }
+
   def rooms
   	@rooms ||= find_rooms
   end
 
 private
   def find_rooms
-  	rooms = Room.order(bar_name)
-  	#rooms = rooms.where("Bar.find(Room.find_by_name(name).bar_id).name = ?", bar_name)
-  	#rooms = rooms.hdctranges.where("min <= ?", hdct) if hdct.present?
-  	#rooms = rooms.hdctranges.where("max >= ?", hdct) if hdct.present?
+  	rooms = Room.order(privacy)
+  	rooms = rooms.joins(:bar).where("neighborhood = ?", location) if location.present?
+  	rooms = rooms.joins(:bar).where("bars.name = ?", bar_name) if bar_name.present?
+  	rooms = rooms.joins(:hdctranges).where("min <= ?", hdct) if hdct.present?
+  	rooms = rooms.joins(:hdctranges).where("max >= ?", hdct) if hdct.present?
   	rooms = rooms.where("privacy = ?", privacy) if privacy.present?
+  	rooms = rooms.joins(:bar).joins(:bar => :hrsranges).where("open <= ? AND #{Date::DAYNAMES[date.wday].downcase} = ?", start_time, true) if start_time.present?
   	rooms
   end
 end
