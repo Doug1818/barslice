f75fa2bfaac85bfcfe23f7fa51980654e2b33033
Doug Raicek <doug.raicek@gmail.com>
2012-07-31 17:00:31 -0400

    Attempt solving time methods with case statement

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,6 +6,12 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def create
+    @bar = Bar.new(params[:room])
+    binding.pry
+    @bar.hrsranges.each_with_index do |hrsrange, idx|
+      hrsrange.open = params[:bar][:hrsranges_attributes].values[idx][:open].time_int
+      hrsrange.close = params[:bar][:hrsranges_attributes].values[idx][:close].time_int
+    end
     super
   end
 
@@ -19,5 +25,113 @@ class RegistrationsController < Devise::RegistrationsController
 
   def destroy
     super
+  end
+
+  def time_int
+    case open
+      when  "6:30 AM" then  630
+      when  "7:00 AM" then  700
+      when  "7:30 AM" then  730
+      when  "8:00 AM" then  800  
+      when  "8:30 AM" then  830
+      when  "9:00 AM" then  900
+      when  "9:30 AM" then  930
+      when "10:00 AM" then 1000
+      when "10:30 AM" then 1030
+      when "11:00 AM" then 1100
+      when "11:30 AM" then 1130
+      when "12:00 PM" then 1200
+      when "12:30 PM" then 1230
+      when  "1:00 PM" then 1300
+      when  "1:30 PM" then 1330
+      when  "2:00 PM" then 1400
+      when  "2:30 PM" then 1430
+      when  "3:00 PM" then 1500
+      when  "3:30 PM" then 1530
+      when  "4:00 PM" then 1600
+      when  "4:30 PM" then 1630
+      when  "5:00 PM" then 1700
+      when  "5:30 PM" then 1730
+      when  "6:00 PM" then 1800
+      when  "6:30 PM" then 1830
+      when  "7:00 PM" then 1900
+      when  "7:30 PM" then 1930
+      when  "8:00 PM" then 2000
+      when  "8:30 PM" then 2030
+      when  "9:00 PM" then 2100
+      when  "9:30 PM" then 2130
+      when "10:00 PM" then 2200
+      when "10:30 PM" then 2230
+      when "11:00 PM" then 2300
+      when "11:30 PM" then 2330
+      when "12:00 AM" then 2400
+      when "12:30 AM" then 2430
+      when  "1:00 AM" then 2500
+      when  "1:30 AM" then 2530
+      when  "2:00 AM" then 2600
+      when  "2:30 AM" then 2630
+      when  "3:00 AM" then 2700
+      when  "3:30 AM" then 2730
+      when  "4:00 AM" then 2800
+      when  "4:30 AM" then 2830
+      when  "5:00 AM" then 2900
+      when  "5:30 AM" then 2930
+      when  "6:00 AM" then 3000
+      else "Unknown"
+    end
+  end
+
+  def time_int
+    case close
+      when  "6:30 AM" then  630
+      when  "7:00 AM" then  700
+      when  "7:30 AM" then  730
+      when  "8:00 AM" then  800  
+      when  "8:30 AM" then  830
+      when  "9:00 AM" then  900
+      when  "9:30 AM" then  930
+      when "10:00 AM" then 1000
+      when "10:30 AM" then 1030
+      when "11:00 AM" then 1100
+      when "11:30 AM" then 1130
+      when "12:00 PM" then 1200
+      when "12:30 PM" then 1230
+      when  "1:00 PM" then 1300
+      when  "1:30 PM" then 1330
+      when  "2:00 PM" then 1400
+      when  "2:30 PM" then 1430
+      when  "3:00 PM" then 1500
+      when  "3:30 PM" then 1530
+      when  "4:00 PM" then 1600
+      when  "4:30 PM" then 1630
+      when  "5:00 PM" then 1700
+      when  "5:30 PM" then 1730
+      when  "6:00 PM" then 1800
+      when  "6:30 PM" then 1830
+      when  "7:00 PM" then 1900
+      when  "7:30 PM" then 1930
+      when  "8:00 PM" then 2000
+      when  "8:30 PM" then 2030
+      when  "9:00 PM" then 2100
+      when  "9:30 PM" then 2130
+      when "10:00 PM" then 2200
+      when "10:30 PM" then 2230
+      when "11:00 PM" then 2300
+      when "11:30 PM" then 2330
+      when "12:00 AM" then 2400
+      when "12:30 AM" then 2430
+      when  "1:00 AM" then 2500
+      when  "1:30 AM" then 2530
+      when  "2:00 AM" then 2600
+      when  "2:30 AM" then 2630
+      when  "3:00 AM" then 2700
+      when  "3:30 AM" then 2730
+      when  "4:00 AM" then 2800
+      when  "4:30 AM" then 2830
+      when  "5:00 AM" then 2900
+      when  "5:30 AM" then 2930
+      when  "6:00 AM" then 3000
+      else "Unknown"
+    end
   end
 end
--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -12,11 +12,15 @@ before_filter :authenticate_bar!, only: [:new, :create, :destroy, :edit, :update
 
   def create
     @room = current_bar.rooms.build(params[:room])
+    #binding.pry
     @room.restrictions.each_with_index do |restriction, idx|
       restriction.start_date = Date.strptime(params[:room][:restrictions_attributes].values[idx][:start_date],"%m/%d/%Y").to_date if !params[:room][:restrictions_attributes].values[idx][:start_date].blank?
       restriction.end_date = Date.strptime(params[:room][:restrictions_attributes].values[idx][:end_date],"%m/%d/%Y").to_date if !params[:room][:restrictions_attributes].values[idx][:end_date].blank?
-      restriction.before = nil if params[:room][:restrictions_attributes].values[idx]["before(4i)"].blank?
-      restriction.after = nil if params[:room][:restrictions_attributes].values[idx]["after(4i)"].blank?
+
+      time_int(restriction.after, params[:room][:restrictions_attributes].values[idx][:after])
+
+#      restriction.before = params[:room][:restrictions_attributes].values[idx][:before].time_int if !params[:room][:restrictions_attributes].values[idx][:before].blank?
+#      restriction.after = params[:room][:restrictions_attributes].values[idx][:after].time_int if !params[:room][:restrictions_attributes].values[idx][:after].blank?
     end if params[:room][:restrictions_attributes]
     if @room.save
       flash[:success] = "Room created"
@@ -73,5 +77,175 @@ before_filter :authenticate_bar!, only: [:new, :create, :destroy, :edit, :update
   end
 
   def index
+  end
+
+  def testmethod
+    if self == "11:00 PM"
+      rtn = 1100
+    else
+      rtn = nil
+    end
+  end
+
+  def time_int(result, value)
+    result = case value
+      when  "6:30 AM" then  630
+      when  "7:00 AM" then  700
+      when  "7:30 AM" then  730
+      when  "8:00 AM" then  800  
+      when  "8:30 AM" then  830
+      when  "9:00 AM" then  900
+      when  "9:30 AM" then  930
+      when "10:00 AM" then 1000
+      when "10:30 AM" then 1030
+      when "11:00 AM" then 1100
+      when "11:30 AM" then 1130
+      when "12:00 PM" then 1200
+      when "12:30 PM" then 1230
+      when  "1:00 PM" then 1300
+      when  "1:30 PM" then 1330
+      when  "2:00 PM" then 1400
+      when  "2:30 PM" then 1430
+      when  "3:00 PM" then 1500
+      when  "3:30 PM" then 1530
+      when  "4:00 PM" then 1600
+      when  "4:30 PM" then 1630
+      when  "5:00 PM" then 1700
+      when  "5:30 PM" then 1730
+      when  "6:00 PM" then 1800
+      when  "6:30 PM" then 1830
+      when  "7:00 PM" then 1900
+      when  "7:30 PM" then 1930
+      when  "8:00 PM" then 2000
+      when  "8:30 PM" then 2030
+      when  "9:00 PM" then 2100
+      when  "9:30 PM" then 2130
+      when "10:00 PM" then 2200
+      when "10:30 PM" then 2230
+      when "11:00 PM" then 2300
+      when "11:30 PM" then 2330
+      when "12:00 AM" then 2400
+      when "12:30 AM" then 2430
+      when  "1:00 AM" then 2500
+      when  "1:30 AM" then 2530
+      when  "2:00 AM" then 2600
+      when  "2:30 AM" then 2630
+      when  "3:00 AM" then 2700
+      when  "3:30 AM" then 2730
+      when  "4:00 AM" then 2800
+      when  "4:30 AM" then 2830
+      when  "5:00 AM" then 2900
+      when  "5:30 AM" then 2930
+      when  "6:00 AM" then 3000
+      else "Unknown"
+    end
+  end
+
+  def temp
+    case before
+      when  "6:30 AM" then  630
+      when  "7:00 AM" then  700
+      when  "7:30 AM" then  730
+      when  "8:00 AM" then  800  
+      when  "8:30 AM" then  830
+      when  "9:00 AM" then  900
+      when  "9:30 AM" then  930
+      when "10:00 AM" then 1000
+      when "10:30 AM" then 1030
+      when "11:00 AM" then 1100
+      when "11:30 AM" then 1130
+      when "12:00 PM" then 1200
+      when "12:30 PM" then 1230
+      when  "1:00 PM" then 1300
+      when  "1:30 PM" then 1330
+      when  "2:00 PM" then 1400
+      when  "2:30 PM" then 1430
+      when  "3:00 PM" then 1500
+      when  "3:30 PM" then 1530
+      when  "4:00 PM" then 1600
+      when  "4:30 PM" then 1630
+      when  "5:00 PM" then 1700
+      when  "5:30 PM" then 1730
+      when  "6:00 PM" then 1800
+      when  "6:30 PM" then 1830
+      when  "7:00 PM" then 1900
+      when  "7:30 PM" then 1930
+      when  "8:00 PM" then 2000
+      when  "8:30 PM" then 2030
+      when  "9:00 PM" then 2100
+      when  "9:30 PM" then 2130
+      when "10:00 PM" then 2200
+      when "10:30 PM" then 2230
+      when "11:00 PM" then 2300
+      when "11:30 PM" then 2330
+      when "12:00 AM" then 2400
+      when "12:30 AM" then 2430
+      when  "1:00 AM" then 2500
+      when  "1:30 AM" then 2530
+      when  "2:00 AM" then 2600
+      when  "2:30 AM" then 2630
+      when  "3:00 AM" then 2700
+      when  "3:30 AM" then 2730
+      when  "4:00 AM" then 2800
+      when  "4:30 AM" then 2830
+      when  "5:00 AM" then 2900
+      when  "5:30 AM" then 2930
+      when  "6:00 AM" then 3000
+      else "Unknown"
+    end
+  end
+
+  def temp
+    case params[:room][:restrictions_attributes].values[idx][:after]
+      when  "6:30 AM" then  630
+      when  "7:00 AM" then  700
+      when  "7:30 AM" then  730
+      when  "8:00 AM" then  800  
+      when  "8:30 AM" then  830
+      when  "9:00 AM" then  900
+      when  "9:30 AM" then  930
+      when "10:00 AM" then 1000
+      when "10:30 AM" then 1030
+      when "11:00 AM" then 1100
+      when "11:30 AM" then 1130
+      when "12:00 PM" then 1200
+      when "12:30 PM" then 1230
+      when  "1:00 PM" then 1300
+      when  "1:30 PM" then 1330
+      when  "2:00 PM" then 1400
+      when  "2:30 PM" then 1430
+      when  "3:00 PM" then 1500
+      when  "3:30 PM" then 1530
+      when  "4:00 PM" then 1600
+      when  "4:30 PM" then 1630
+      when  "5:00 PM" then 1700
+      when  "5:30 PM" then 1730
+      when  "6:00 PM" then 1800
+      when  "6:30 PM" then 1830
+      when  "7:00 PM" then 1900
+      when  "7:30 PM" then 1930
+      when  "8:00 PM" then 2000
+      when  "8:30 PM" then 2030
+      when  "9:00 PM" then 2100
+      when  "9:30 PM" then 2130
+      when "10:00 PM" then 2200
+      when "10:30 PM" then 2230
+      when "11:00 PM" then 2300
+      when "11:30 PM" then 2330
+      when "12:00 AM" then 2400
+      when "12:30 AM" then 2430
+      when  "1:00 AM" then 2500
+      when  "1:30 AM" then 2530
+      when  "2:00 AM" then 2600
+      when  "2:30 AM" then 2630
+      when  "3:00 AM" then 2700
+      when  "3:30 AM" then 2730
+      when  "4:00 AM" then 2800
+      when  "4:30 AM" then 2830
+      when  "5:00 AM" then 2900
+      when  "5:30 AM" then 2930
+      when  "6:00 AM" then 3000
+      else "Unknown"
+    end
   end
 end
