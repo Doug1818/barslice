98134079e219147e55e8866a091da0d595b6630e
Doug Raicek <doug.raicek@gmail.com>
2012-08-01 01:38:25 -0400

    Add time changes to SeachesController

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -6,6 +6,8 @@ class SearchesController < ApplicationController
   def create
     @search = Search.new(params[:search])
   	@search.date = Date.strptime(params[:search][:date],"%m/%d/%Y").to_time
+    @search.start_time = TIMES[params[:search][:start_time]]
+    @search.end_time = TIMES[params[:search][:end_time]]
     @search.save
     redirect_to @search
   end
