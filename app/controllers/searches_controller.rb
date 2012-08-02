a6b8f6181a8e4a0369559e051d0f5d4732c9e869
Doug Raicek <doug.raicek@gmail.com>
2012-08-02 12:15:17 -0400

    Move search form to home page

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,14 +5,34 @@ class SearchesController < ApplicationController
 
   def create
     @search = Search.new(params[:search])
-  	@search.date = Date.strptime(params[:search][:date],"%m/%d/%Y").to_time
+  	@search.date = Date.strptime(params[:search][:date],"%m/%d/%Y").to_date
     @search.start_time = TIMES[params[:search][:start_time]]
     @search.end_time = TIMES[params[:search][:end_time]]
     @search.save
-    redirect_to @search
+    redirect_to search_path(@search)
   end
 
   def show
-  	@search = Search.find(params[:id])
+  	@edit_search = Search.find(params[:id])
+    @edit_search.date = @edit_search.date.strftime("%m/%d/%Y")
+    @edit_search.start_time = TIMES.key(@edit_search.start_time)
+    @edit_search.end_time = TIMES.key(@edit_search.end_time)
+    @show_search = Search.find(params[:id])
+  end
+
+  def edit
+    #@search = Search.find(params[:id])
+    #@search.date = @search.date.strftime("%m/%d/%Y")
+    #@search.start_time = TIMES.key(@search.start_time)
+    #@search.end_time = TIMES.key(@search.end_time)
+  end
+
+  def update
+    @search = Search.find(params[:id])
+    params[:search][:date] = Date.strptime(params[:search][:date],"%m/%d/%Y").to_date
+    params[:search][:start_time] = TIMES[params[:search][:start_time]]
+    params[:search][:end_time] = TIMES[params[:search][:end_time]]
+    @search.update_attributes(params[:search])
+    redirect_to search_path(@search)
   end
 end
