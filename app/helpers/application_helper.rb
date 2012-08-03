672a66d5806db01cf2da7fd089b8866d7ecbcdb4
Doug Raicek <doug.raicek@gmail.com>
2012-08-03 10:22:47 -0400

    Turn bar url to link on views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,8 +10,6 @@ module ApplicationHelper
 		end
 	end
 
-	
-
 	def link_to_add_fields(name, f, association)
 	    new_object = f.object.send(association).klass.new
 	    id = new_object.object_id
@@ -19,5 +17,9 @@ module ApplicationHelper
 	      render(association.to_s.singularize + "_fields", f: builder)
 	    end
     	link_to(name, '#', class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")})
+  	end
+
+  	def url_with_protocol(url)
+    	/^http/.match(url) ? url : "http://#{url}"
   	end
 end
