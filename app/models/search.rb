a5409836da24b87afebbbe20c24aaeb314edf321
Doug Raicek <doug.raicek@gmail.com>
2012-08-17 01:30:32 -0400

    Fix min ambiguity in search model

--- app/models/search.rb
+++ app/models/search.rb
@@ -29,14 +29,14 @@ class Search < ActiveRecord::Base
 private
   def find_specials
     specials = Special.order(:name)
-    good_time = specials.where("specials.from <= ? AND until >= ? AND specials.#{Date::DAYNAMES[date.wday].downcase} = ?
+    good_time = specials.where("specials.from <= ? AND specials.until >= ? AND specials.#{Date::DAYNAMES[date.wday].downcase} = ?
       AND specials.start_date IS NULL AND specials.end_date IS NULL", start_time, end_time, true) if date.present?
     good_date = specials.where("specials.start_date <= ? AND specials.end_date >= ?
-      AND specials.#{Date::DAYNAMES[date.wday].downcase} = ? AND specials.from IS NULL AND until IS NULL", date, date, true) if date.present?
-    good_date_and_time = specials.where("specials.from <= ? AND until >= ? AND specials.start_date <= ? AND specials.end_date >= ?
+      AND specials.#{Date::DAYNAMES[date.wday].downcase} = ? AND specials.from IS NULL AND specials.until IS NULL", date, date, true) if date.present?
+    good_date_and_time = specials.where("specials.from <= ? AND specials.until >= ? AND specials.start_date <= ? AND specials.end_date >= ?
       AND specials.#{Date::DAYNAMES[date.wday].downcase} = ?", start_time, end_time, date, date, true) if date.present?
     good_day = specials.where("specials.#{Date::DAYNAMES[date.wday].downcase} = ?
-      AND specials.start_date IS NULL AND specials.end_date IS NULL AND specials.from IS NULL AND until IS NULL", true) if date.present?
+      AND specials.start_date IS NULL AND specials.end_date IS NULL AND specials.from IS NULL AND specials.until IS NULL", true) if date.present?
     specials = good_time + good_date + good_date_and_time + good_day if !good_time.nil? && !good_date.nil? && !good_date_and_time.nil? && !good_day.nil?
     specials = specials.to_a.uniq
     specials
@@ -46,13 +46,13 @@ private
   	rooms = Room.order(:name)
   	rooms = rooms.joins(:bar).where("neighborhood = ?", location) if location.present?
   	rooms = rooms.joins(:bar).where("bars.name = ?", bar_name) if bar_name.present?
-  	rooms = rooms.joins(:bar).joins(:bar => :hrsranges).where("open <= ? AND close >= ? AND hrsranges.#{Date::DAYNAMES[date.wday].downcase} = ?",
-      start_time, end_time, true) if date.present?
-    rooms = rooms.joins(:hdctranges).where("min <= ? AND max >= ? AND hdctranges.#{Date::DAYNAMES[date.wday].downcase} = ?",
-      hdct, hdct, true) if hdct.present? && date.present?
+  	rooms = rooms.joins(:bar).joins(:bar => :hrsranges).where("open <= ? AND close >= ? 
+      AND hrsranges.#{Date::DAYNAMES[date.wday].downcase} = ?", start_time, end_time, true) if date.present?
+    rooms = rooms.joins(:hdctranges).where("hdctranges.min <= ? AND hdctranges.max >= ? 
+      AND hdctranges.#{Date::DAYNAMES[date.wday].downcase} = ?", hdct, hdct, true) if hdct.present? && date.present?
     rooms = rooms.where("privacy = ?", privacy) if privacy.present? && date.present?
     rooms = rooms.joins(:fees).where("amount IS NULL") if no_fee.present?
-    rooms = rooms.joins(:spendmins).where("min IS NULL") if no_spendmin.present?
+    rooms = rooms.joins(:spendmins).where("hdctranges.min IS NULL") if no_spendmin.present?
     rooms = rooms.joins(:specials).where("open_bar = ?", true) if has_open_bar.present?
 
     bad_time = rooms.joins(:restrictions).where("(before > ? OR after < ?) AND restrictions.#{Date::DAYNAMES[date.wday].downcase} = ?
