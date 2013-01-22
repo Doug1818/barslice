f02679aa315af2cfb65b1f03ae10c7762f668593
Doug Raicek <doug.raicek@gmail.com>
2013-01-22 12:46:40 -0500

    Remove 'default_charset' from production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,8 +67,6 @@ BarSlice::Application.configure do
 
   # Mailgun smtp settings
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_charset = "utf-8"
-  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'], 
