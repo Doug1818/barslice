2eb23fd9369d22535434586fb543fceaa488cbc5
Doug Raicek <doug.raicek@gmail.com>
2013-01-29 18:45:01 -0500

    Fix before_save bug in hdctrange.rb

--- app/models/hdctrange.rb
+++ app/models/hdctrange.rb
@@ -3,7 +3,7 @@ class Hdctrange < ActiveRecord::Base
   belongs_to :room
 
   before_save do |hdctrange|
-  	if hdctrange.min? && hdctrange.max? && !hdctrange.sunday && !hdctrange.monday && !hdctrange.tuesday && !hdctrange.wednesday && !hdctrange.thursday && !hdctrange.friday && !hdctrange.saturday
+  	if hdctrange.min != nil && hdctrange.max != nil && !hdctrange.sunday && !hdctrange.monday && !hdctrange.tuesday && !hdctrange.wednesday && !hdctrange.thursday && !hdctrange.friday && !hdctrange.saturday
   		hdctrange.sunday = true && hdctrange.monday = true && hdctrange.tuesday = true && hdctrange.wednesday = true && hdctrange.thursday = true && hdctrange.friday = true && hdctrange.saturday = true
 	end
   end
