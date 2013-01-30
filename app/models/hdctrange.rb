5b8f639ef24c8b75ee43c9c45d7a162baad18784
Doug Raicek <doug.raicek@gmail.com>
2013-01-29 22:47:49 -0500

    Fix hdctrange before save and week_days validation to account for closed days

--- app/models/hdctrange.rb
+++ app/models/hdctrange.rb
@@ -4,8 +4,27 @@ class Hdctrange < ActiveRecord::Base
 
   before_save do |hdctrange|
   	if hdctrange.min != nil && hdctrange.max != nil && !hdctrange.sunday && !hdctrange.monday && !hdctrange.tuesday && !hdctrange.wednesday && !hdctrange.thursday && !hdctrange.friday && !hdctrange.saturday
-  		hdctrange.sunday = true && hdctrange.monday = true && hdctrange.tuesday = true && hdctrange.wednesday = true && hdctrange.thursday = true && hdctrange.friday = true && hdctrange.saturday = true
-	end
+	  	hdctrange.sunday = true && hdctrange.monday = true && hdctrange.tuesday = true && hdctrange.wednesday = true && hdctrange.thursday = true && hdctrange.friday = true && hdctrange.saturday = true
+	  	x = Hash.new
+	  	Bar.find(Room.find(hdctrange.room_id).bar_id).hrsranges.each_with_index do |hrsrange, idx|
+	  		if hrsrange.closed
+		  		if hrsrange.sunday    then x[0]=1 end
+		  		if hrsrange.monday    then x[1]=1 end
+		  		if hrsrange.tuesday   then x[2]=1 end
+	  			if hrsrange.wednesday then x[3]=1 end
+  				if hrsrange.thursday  then x[4]=1 end
+					if hrsrange.friday    then x[5]=1 end
+					if hrsrange.saturday  then x[6]=1 end
+				end
+			end
+			if x[0]==1 then hdctrange.sunday = nil end
+			if x[1]==1 then hdctrange.monday = nil end
+			if x[2]==1 then hdctrange.tuesday = nil end
+			if x[3]==1 then hdctrange.wednesday = nil end
+			if x[4]==1 then hdctrange.thursday = nil end
+			if x[5]==1 then hdctrange.friday = nil end
+			if x[6]==1 then hdctrange.saturday = nil end
+		end
   end
 
   validates :min, presence: true, :numericality => { :only_integer => true }
--- app/models/room.rb
+++ app/models/room.rb
@@ -27,22 +27,35 @@ class Room < ActiveRecord::Base
   end
 
   def week_days
-  	count = []
+  	hdct_days = []
   	self.hdctranges.each_with_index do |hdctrange, idx|
-  		hdctrange.sunday ?    sun = 1   : sun = 0
-  		hdctrange.monday ?    mon = 1   : mon = 0
-  		hdctrange.tuesday ?   tues = 1  : tues = 0
-  		hdctrange.wednesday ? wed = 1   : wed = 0
-  		hdctrange.thursday ?  thurs = 1 : thurs = 0
-  		hdctrange.friday ?    fri = 1   : fri = 0
-  		hdctrange.saturday ?  sat = 1   : sat = 0
-  		count[idx] = sun + mon + tues + wed + thurs + fri + sat
+  		hdctrange.sunday ?    sun = 1 : sun = 0
+  		hdctrange.monday ?    mon = 1 : mon = 0
+  		hdctrange.tuesday ?   tue = 1 : tue = 0
+  		hdctrange.wednesday ? wed = 1 : wed = 0
+  		hdctrange.thursday ?  thu = 1 : thu = 0
+  		hdctrange.friday ?    fri = 1 : fri = 0
+  		hdctrange.saturday ?  sat = 1 : sat = 0
+  		hdct_days[idx] = sun + mon + tue + wed + thu + fri + sat
     end
   	
-    x = []
-    Bar.find(self.bar_id).hrsranges.each { |h| x.push(1) if h.closed }
-
-    if count.inject(:+) != 7 && count.inject(:+) != 0 && count.inject(:+) != x.inject(:+)
+    closed_days = []
+    Bar.find(self.bar_id).hrsranges.each_with_index do |hrsrange, idx|
+      if hrsrange.closed
+        hrsrange.sunday ?    sun = 1 : sun = 0
+        hrsrange.monday ?    mon = 1 : mon = 0
+        hrsrange.tuesday ?   tue = 1 : tue = 0
+        hrsrange.wednesday ? wed = 1 : wed = 0
+        hrsrange.thursday ?  thu = 1 : thu = 0
+        hrsrange.friday ?    fri = 1 : fri = 0
+        hrsrange.saturday ?  sat = 1 : sat = 0
+        closed_days[idx] = sun + mon + tue + wed + thu + fri + sat
+      else
+        closed_days[idx] = 0
+      end
+    end
+    
+    if hdct_days.inject(:+) != 7 && hdct_days.inject(:+) != 0 && hdct_days.inject(:+) != ( 7 - closed_days.inject(:+) )
   	  self.errors[:each_weekday] << "for Number of people must be selected once"
   	end
   end
