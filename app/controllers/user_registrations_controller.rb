59aee1f22496a28b3b02efe2adb969e27ad30bc1
Doug Raicek <doug.raicek@gmail.com>
2013-02-26 18:36:14 -0500

    Add user/bar/room names to user registrations controller

--- app/controllers/user_registrations_controller.rb
+++ app/controllers/user_registrations_controller.rb
@@ -14,6 +14,9 @@ class UserRegistrationsController < Devise::RegistrationsController
     @user.reservations.last.date = Date.strptime(params[:user][:reservations_attributes].values[0][:date],"%m/%d/%Y").to_date if params[:user][:reservations_attributes].values[0][:date] != ""
     @user.reservations.last.start_time = TIMES[params[:user][:reservations_attributes].values[0][:start_time]]
     @user.reservations.last.end_time = TIMES[params[:user][:reservations_attributes].values[0][:end_time]]
+    @user.reservations.last.user_name = @user.name
+    @user.reservations.last.room_name = @room.name
+    @user.reservations.last.bar_name = @bar.name
     if @user.save
       if @bar.claimed == true
         flash[:success] = "Your reservation request has been sent to #{@bar.name}. You will be notified of their availability by email as soon as possible."
